84ce761d440f3ab7919122a0cd07b76cecc5d61e
Kirill Nikolaev <kf@exchange12rocks.org>
2017-02-09 22:56:58 +0300

    Update to best practices
    Introduces a couple of parameter comments.
    Brings the script in compliance with PS3.0 best practices.

--- Allow-ADDomainJoin.ps1
+++ Allow-ADDomainJoin.ps1
@@ -1,24 +1,25 @@
-﻿<#MIT License
+﻿<# MIT License
 
-Copyright (c) 2016 Kirill Nikolaev
+    Copyright (c) 2016 Kirill Nikolaev
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE. #>
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+#>
 
 #Requires -Version 3.0
 #Requires -Modules ActiveDirectory
@@ -57,52 +58,52 @@ https://exchange12rocks.org/2016/03/14/how-to-allow-users-to-join-their-computer
 #>
 
 Param(
-	[Parameter(Mandatory=$True)]
+	[Parameter(Mandatory,
+	HelpMessage='sAMAccount name of a computer account at which you like to delegate domain-joining permissions')]
 	[string]$ComputerName,
-	[Parameter(Mandatory=$True)]
+	[Parameter(Mandatory,
+	HelpMessage='sAMAccount name of a user account to which you like to delegate domain-joining permissions')]
 	[string]$UserName,
-	[Parameter(Mandatory=$False)]
 	[string]$ComputerDomain = 'example.com',
-	[Parameter(Mandatory=$False)]
 	[string]$UserDomain = 'example.com'
 )
 
 $ErrorActionPreference = 'Stop'
 
-Import-Module ActiveDirectory
+Import-Module -Name ActiveDirectory
 
 [string]$ComputerWDC = (Get-ADDomainController -DomainName $ComputerDomain -Discover -Writable -NextClosestSite).HostName
 [string]$UserWDC = (Get-ADDomainController -DomainName $UserDomain -Discover -Writable -NextClosestSite).HostName
-[string]$ComputerDomainNB = (Get-ADDomain $ComputerDomain).NetBIOSName
+[string]$ComputerDomainNB = (Get-ADDomain -Identity $ComputerDomain).NetBIOSName
 
 Try {
-	$ADObject = Get-ADComputer $ComputerName -Server $ComputerWDC
+	$ADObject = Get-ADComputer -Identity $ComputerName -Server $ComputerWDC
 }
 Catch {
-	Write-Error "Cannot get computer $ComputerName from AD DS @ $($ComputerWDC):`n$($Error[0].Exception.Message)`n$($Error[0].InvocationInfo.PositionMessage)"
+	Write-Error -Message ("Cannot get computer {0} from AD DS @ {1}:`r`n{2}`r`n{3}" -f $ComputerName, $ComputerWDC, $Error[0].Exception.Message, $Error[0].InvocationInfo.PositionMessage)
 	Exit
 }
 
 Try {
-    New-PSDrive -Name $ComputerDomainNB -Root '' -PsProvider ActiveDirectory -Server $ComputerWDC | Out-Null
+    $null = New-PSDrive -Name $ComputerDomainNB -Root '' -PSProvider ActiveDirectory -Server $ComputerWDC
 }
 Catch {
-    Write-Error "Cannot create new PSDrive for $ComputerDomain AD domain @ $($ComputerWDC):`n$($Error[0].Exception.Message)`n$($Error[0].InvocationInfo.PositionMessage)"
+    Write-Error -Message ("Cannot create new PSDrive for {0} AD domain @ {1}:`r`n{2}`r`n{3}" -f $ComputerDomain, $ComputerWDC, $Error[0].Exception.Message, $Error[0].InvocationInfo.PositionMessage)
     Exit
 }
 
-Try {
-	$DACL = Get-Acl -Path "$($ComputerDomainNB):\$($ADObject.DistinguishedName)"
+Try { #TODO: Get-Acl desn't support ShouldProcess
+	$DACL = Get-Acl -Path ('{0}:\{1}' -f $ComputerDomainNB, $ADObject.DistinguishedName)
 }
 Catch {
-	Write-Error "Cannot get DACL for object $($ADObject.DistinguishedName) @ $($ComputerWDC):`n$($Error[0].Exception.Message)`n$($Error[0].InvocationInfo.PositionMessage)"
+	Write-Error -Message ("Cannot get DACL for object {0} @ {1}:`r`n{2}`r`n{3}" -f $ADObject.DistinguishedName, $ComputerWDC, $Error[0].Exception.Message, $Error[0].InvocationInfo.PositionMessage)
 	Exit
 }
 Try {
-	$UserSID = (Get-ADUser $UserName -Properties SID -Server $UserWDC).SID
+	$UserSID = (Get-ADUser -Identity $UserName -Properties SID -Server $UserWDC).SID
 }
 Catch {
-	Write-Error "Cannot get user $UserName from AD DS @ $($UserWDC):`n$($Error[0].Exception.Message)`n$($Error[0].InvocationInfo.PositionMessage)"
+	Write-Error -Message ("Cannot get user {0} from AD DS @ {1}:`r`n{2}`r`n{3}" -f $UserName, $UserWDC, $Error[0].Exception.Message, $Error[0].InvocationInfo.PositionMessage)
 	Exit
 }
 
@@ -110,22 +111,22 @@ Try {
 	$ACEs = @()
 }
 Catch {
-	Write-Error "Cannot initialize empty ACEs object:`n$($Error[0].Exception.Message)`n$($Error[0].InvocationInfo.PositionMessage)"
+	Write-Error -Message ("Cannot initialize empty ACEs object:`r`n{0}`r`n{1}" -f $Error[0].Exception.Message, $Error[0].InvocationInfo.PositionMessage)
 	Exit
 }
 
 Try {
-	$ACEs += New-Object System.DirectoryServices.ActiveDirectoryAccessRule ($UserSID,'197076', 'Allow', [GUID]'00000000-0000-0000-0000-000000000000', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
-	$ACEs += New-Object System.DirectoryServices.ActiveDirectoryAccessRule ($UserSID,'WriteProperty', 'Allow', [GUID]'3e0abfd0-126a-11d0-a060-00aa006c33ed', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
-	$ACEs += New-Object System.DirectoryServices.ActiveDirectoryAccessRule ($UserSID,'WriteProperty', 'Allow', [GUID]'bf967953-0de6-11d0-a285-00aa003049e2', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
-	$ACEs += New-Object System.DirectoryServices.ActiveDirectoryAccessRule ($UserSID,'WriteProperty', 'Allow', [GUID]'bf967950-0de6-11d0-a285-00aa003049e2', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
-	$ACEs += New-Object System.DirectoryServices.ActiveDirectoryAccessRule ($UserSID,'WriteProperty', 'Allow', [GUID]'5f202010-79a5-11d0-9020-00c04fc2d4cf', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
-	$ACEs += New-Object System.DirectoryServices.ActiveDirectoryAccessRule ($UserSID,'WriteProperty', 'Allow', [GUID]'4c164200-20c0-11d0-a768-00aa006e0529', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
-	$ACEs += New-Object System.DirectoryServices.ActiveDirectoryAccessRule ($UserSID,'Self', 'Allow', [GUID]'f3a64788-5306-11d1-a9c5-0000f80367c1', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
-	$ACEs += New-Object System.DirectoryServices.ActiveDirectoryAccessRule ($UserSID,'Self', 'Allow', [GUID]'72e39547-7b18-11d1-adef-00c04fd8d5cd', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
+	$ACEs += New-Object -TypeName System.DirectoryServices.ActiveDirectoryAccessRule -ArgumentList ($UserSID,'197076', 'Allow', [GUID]'00000000-0000-0000-0000-000000000000', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
+	$ACEs += New-Object -TypeName System.DirectoryServices.ActiveDirectoryAccessRule -ArgumentList ($UserSID,'WriteProperty', 'Allow', [GUID]'3e0abfd0-126a-11d0-a060-00aa006c33ed', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
+	$ACEs += New-Object -TypeName System.DirectoryServices.ActiveDirectoryAccessRule -ArgumentList ($UserSID,'WriteProperty', 'Allow', [GUID]'bf967953-0de6-11d0-a285-00aa003049e2', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
+	$ACEs += New-Object -TypeName System.DirectoryServices.ActiveDirectoryAccessRule -ArgumentList ($UserSID,'WriteProperty', 'Allow', [GUID]'bf967950-0de6-11d0-a285-00aa003049e2', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
+	$ACEs += New-Object -TypeName System.DirectoryServices.ActiveDirectoryAccessRule -ArgumentList ($UserSID,'WriteProperty', 'Allow', [GUID]'5f202010-79a5-11d0-9020-00c04fc2d4cf', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
+	$ACEs += New-Object -TypeName System.DirectoryServices.ActiveDirectoryAccessRule -ArgumentList ($UserSID,'WriteProperty', 'Allow', [GUID]'4c164200-20c0-11d0-a768-00aa006e0529', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
+	$ACEs += New-Object -TypeName System.DirectoryServices.ActiveDirectoryAccessRule -ArgumentList ($UserSID,'Self', 'Allow', [GUID]'f3a64788-5306-11d1-a9c5-0000f80367c1', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
+	$ACEs += New-Object -TypeName System.DirectoryServices.ActiveDirectoryAccessRule -ArgumentList ($UserSID,'Self', 'Allow', [GUID]'72e39547-7b18-11d1-adef-00c04fd8d5cd', 'None', [GUID]'00000000-0000-0000-0000-000000000000')
 }
 Catch {
-	Write-Error "Error while creating ACE objects:`n$($Error[0].Exception.Message)`n$($Error[0].InvocationInfo.PositionMessage)"
+	Write-Error -Message ("Error while creating ACE objects:`r`n{0}`r`n{1}" -f $Error[0].Exception.Message, $Error[0].InvocationInfo.PositionMessage)
 	Exit
 }
 
@@ -135,15 +136,15 @@ Try {
 	}
 }
 Catch {
-	Write-Error "Error while populating DACL with ACE objects:`n$($Error[0].Exception.Message)`n$($Error[0].InvocationInfo.PositionMessage)"
+	Write-Error -Message ("Error while populating DACL with ACE objects:`r`n{0}`r`n{1}" -f $Error[0].Exception.Message, $Error[0].InvocationInfo.PositionMessage)
 	Exit
 }
 Try {
-	Set-Acl -AclObject $DACL -Path "$($ComputerDomainNB):\$($ADObject.DistinguishedName)"
+	Set-Acl -AclObject $DACL -Path ('{0}:\{1}' -f $ComputerDomainNB, $ADObject.DistinguishedName)
 }
 Catch {
-	Write-Error "Error while writing DACL back to $($ADObject.DistinguishedName) @ $($ComputerWDC):`n$($Error[0].Exception.Message)`n$($Error[0].InvocationInfo.PositionMessage)"
+	Write-Error -Message ("Error while writing DACL back to {0} @ {1}:`r`n{2}`n{3}" -f $ADObject.DistinguishedName, $ComputerWDC, $Error[0].Exception.Message, $Error[0].InvocationInfo.PositionMessage)
 	Exit
 }
 
-Write-Output "$UserName is now allowed to join $ComputerName to the domain $ComputerDomain."
+Write-Output -InputObject ('{0} is now allowed to join {1} to the domain {2}.' -f $UserName, $ComputerName, $ComputerDomain)
